a490154fc5e8a8275cee7968b676a92d79edd78d
RoyaAmirii <roya.amiri@ada.ac.uk>
2025-02-09 20:15:14 +0000

    equipment management

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -3,12 +3,14 @@
 .mode column
 
 -- Enable foreign key support
-
+PRAGMA foreign_keys = ON;
 -- Equipment Management Queries
 
 -- 1. Find equipment due for maintenance
 -- TODO: Write a query to find equipment due for maintenance
-
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN DATE('now') AND DATE('now', '+30 days'); --filters equipment that needs maintenance within the next 30 days
 -- 2. Count equipment types in stock
 -- TODO: Write a query to count equipment types in stock
 
