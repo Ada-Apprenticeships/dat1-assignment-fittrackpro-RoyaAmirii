3b2e8ba20f4a11064fbef9a24f16e098de0e0256
RoyaAmirii <roya.amiri@ada.ac.uk>
2025-02-19 11:05:03 +0000

    final commit

--- src/payment_management.sql
+++ src/payment_management.sql
@@ -3,6 +3,7 @@
 .mode column
 
 -- Enable foreign key support
+--PRAGMA foreign_key_list('payments');
 PRAGMA foreign_keys = ON;
 -- Payment Management Queries
 
@@ -13,6 +14,16 @@ VALUES (11, 50.00, CURRENT_TIMESTAMP, 'Credit Card', 'Monthly membership fee'); 
 
 -- 2. Calculate total revenue from membership fees for each month of the last year
 -- TODO: Write a query to calculate total revenue from membership fees for each month of the current year
-
+SELECT 
+    strftime('%Y-%m', payment_date) AS month, -- strftime('%Y-%m') extracts the year and month from the payment_date 
+    SUM(amount) AS total_revenue --calculates the total revenue for each month
+FROM payments
+WHERE payment_type = 'Monthly membership fee' -- used this to filter only 'Monthly membership fee' payments and to limit results to the past year.
+    AND payment_date >= date('now', '-1 year')
+GROUP BY month
+ORDER BY month DESC;  -- put in descending order to show the most recent months first
 -- 3. Find all day pass purchases
 -- TODO: Write a query to find all day pass purchases
+SELECT payment_id, amount, payment_date, payment_method
+FROM payments
+WHERE payment_type = 'Day Pass';
